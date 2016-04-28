f7228408123c1c8287813b1c6944f9f30799582b
Chesco95 <franciscovall95@gmail.com>
2016-04-28 17:01:43 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,8 +1,9 @@
 import socket
 import sys
 def pro():
+    usuarios="[[1,'Vero'],[2,'Fran'],[13,'Maria']]"
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 10000)
+    server_address = ('', 10000)
     print("Iniciando servidor en el puerto",server_address)
     sock.bind(server_address)
     sock.listen(1)
@@ -12,11 +13,12 @@ def pro():
         try:
             print("Conexión desde",client_address)
             while True:
-                data = connection.recv(19)
+                data = connection.recv(100)
                 print(data.decode(encoding='UTF-8'))
                 if data:
                     print("Enviando mensaje de vuelta")
-                    connection.sendall(data)
+                    usuarios=usuarios.encode(encoding='UTF-8')
+                    connection.sendall(usuarios)
                 else:
                     print("No hay más datos")
                     break
