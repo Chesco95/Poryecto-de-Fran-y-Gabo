ed68c1980db8f87b810a58a68b1b5d006d3b7ca3
Paradox6797 <gabrielz6797@yahoo.com>
2016-04-28 17:02:02 -0600

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,7 +2,8 @@ import socket
 import sys
 def pro():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 10000)
+    global sock
+    server_address = ('172.19.13.78', 10000)
     print("Conectando al puerto", server_address)
     sock.connect(server_address)
     try:
@@ -13,9 +14,40 @@ def pro():
         amount_received = 0
         amount_expected = len(message)
         while amount_received < amount_expected:
-            data = sock.recv(19)
+            data = sock.recv(100)
             amount_received += len(data)
-            print("Resiviendo",message.decode(encoding='UTF-8'))
+            print("Resiviendo",data.decode(encoding='UTF-8'))
+            usuarios = data.decode(encoding='UTF-8')
+            usuarios = eval(usuarios)
+            #inventario
+            menu()
+            
     finally:
-        print("Cerrando socket")
-        sock.close()
+        #sal = input("¿Salir? (S/N): ")
+        #if sal == "S":
+            print("Cerrando socket")
+            sock.close()
+        #else:
+            #menu()
+
+def menu():
+    print("1 - Iniciar Seción")
+    print("2 - Solicitar Registro")
+    print("3 - Realizar Compra")
+    print("4 - Solicitar Facturación")
+    print("5 - Eliminar Producto")
+    print("6 - Salir del sistema")
+    op = input("Seleción: ")
+    if op == "1":
+        menu()
+    elif op == "2":
+        menu()
+    elif op == "3":
+        menu()
+    elif op == "4":
+        menu()
+    elif op == "5":
+        menu()
+    elif op == "6":
+        print("Adios")
+
